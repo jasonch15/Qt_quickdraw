e46510ea95dabeab55ddba6202552491b91e7c09
jasonch15 <101620411+jasonch15@users.noreply.github.com>
2024-12-26 23:14:57 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    MainWindow mainWindow;
-    mainWindow.show();
-    return app.exec();
-}
